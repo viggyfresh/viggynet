f72b6ccb40632eaa85430b01e4ca324127647ca0
Neal Khosla <nealk@stanford.edu>
2015-03-13 22:32:54 -0400

    Fixed stuff

--- extract_features.sh
+++ extract_features.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 ./build/tools/extract_features.bin \
-./examples/viggynet/snapshot_iter_2000.caffemodel \
-./examples/viggynet/shoes_feature_extract.prototxt \
-fc1 \
+./examples/viggynet/vgg.caffemodel \
+./examples/viggynet/vgg.prototxt \
+fc6 \
 ./examples/viggynet/features \
 237 \
 lmdb \
