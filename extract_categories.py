979d17a73aab506b4e1bd76482ec4cc08f6f523f
Vignesh Venkataraman <sendittovignesh@gmail.com>
2015-05-29 16:30:05 -0700

    Added reverse mapping tooo

--- extract_categories.py
+++ extract_categories.py
@@ -1,16 +1,20 @@
 import pickle
 import numpy as np
 
-map = {}
+cat_map = {}
+rev_map = {}
 for i in xrange(12):
-    map[i] = []
+    cat_map[i] = []
 counter = 0
 with open('labels.txt') as file:
     for line in file:
         parts = line.split()
         id = int(parts[0][:-4])
         cat = int(parts[1])
-        map[cat].append(id)
+        cat_map[cat].append(id)
+        rev_map[id] = cat
         counter += 1
     with open('cat_map.pickle', 'wb') as outfile:
-        pickle.dump(map, outfile)
+        pickle.dump(cat_map, outfile)
+    with open('rev_map.pickle', 'wb') as outfile:
+        pickle.dump(rev_map, outfile)
